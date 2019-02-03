6ece22e6e878c31c602cd60373d0e17a19b5def3
sattsu55 <owner@ownernoMacBook-puro.local>
2019-02-03 18:03:25 +0900

    kaminari設定

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,7 +2,7 @@ class BlogsController < ApplicationController
 	before_action :move_to_index, except: :index
 
 	def index
-		@blog = Blog.all.order("id DESC")
+		@blog = Blog.all.order("id DESC").page(params[:page]).per(5)
 	end
 
     def create
