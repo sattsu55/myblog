845d374afcb2bda6568d96b6bbad3b41fc406005
sattsu55 <owner@ownernoMacBook-puro.local>
2019-02-04 17:21:31 +0900

    form_withこっちの方がいい

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
 	def create
-		Comment.create(text: comment_params[:text],blog_id: comment_params[:blog_id],user_id: current_user.id)
+		Comment.create(comment_params)
 	end
 
 	private
 	def comment_params
-		params.permit(:text,:blog_id)
+		params.require(:comment).permit(:text).merge(blog_id: params[:blog_id],user_id: current_user.id)
 	end
 end
